7a94ac1b42b791c2c5b16eca4de0f750ba206a44
LilDevilsprout <sara.astuti.aucher@gmail.com>
2026-03-12 21:22:40 +0100

    Added 5 animations

--- docs/main.js
+++ docs/main.js
@@ -50,94 +50,103 @@ function toggleAnswer(element) {
 }
 
 // FORM
+
 let form = document.querySelector("form")
 form.addEventListener("submit", function(event) {
     event.preventDefault();
-    console.log("Envoi du form détecté !")
+    let loader = document.querySelector(".loader");
+    loader.classList.add("visible");
 
-    let email = document.querySelector("#email")
-    let pseudo = document.querySelector("#pseudo")
-    let password = document.querySelector("#password")
-    let passwordRepeat = document.querySelector("#password2")
-    let errorContainer = document.querySelector(".message-error")
-    let successContainer = document.querySelector(".message-succes")
-    let errorList = document.querySelector(".message-error")
-    let newsletterOui = document.querySelector("#oui")
-    let newsletterNon = document.querySelector("#non")
+    setTimeout(function(){
+        console.log("Envoi du form détecté !")
 
-    errorList.replaceChildren()
-    errorContainer.classList.remove("visible")
-    successContainer.classList.remove("visible")
+        let email = document.querySelector("#email")
+        let pseudo = document.querySelector("#pseudo")
+        let password = document.querySelector("#password")
+        let passwordRepeat = document.querySelector("#password2")
+        let errorContainer = document.querySelector(".message-error")
+        let successContainer = document.querySelector(".message-succes")
+        let errorList = document.querySelector(".message-error")
+        let newsletterOui = document.querySelector("#oui")
+        let newsletterNon = document.querySelector("#non")
 
-    if (email.value == "") {
-        console.log("invalide")
-    } else {
-        email.classList.add("success")
-    }
-
-    if (pseudo.value.length < 6) {
-        errorContainer.classList.add("visible");
-        pseudo.classList.remove("success");
-
-        let err = document.createElement("li");
-        err.innerText = "Le champ pseudo doit contenir au moins 6 caractères";
-
-        errorList.appendChild(err);
-    } else {
-        pseudo.classList.add("success")
-    }
-
-    let passCheck = new RegExp(
-    "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$");
-
-    // Si le mdp n"a pas 10 caractère ou ne respecte pas la regex
-    if (password.value.length < 10 || passCheck.test(password.value) == false) {
-        errorContainer.classList.add("visible");
-        password.classList.remove("success");
-
-        let err = document.createElement("li");
-        err.innerText = "Le mot de passe doit faire 10 caractères minimum, contenir minuscule, majuscule, chiffre, caractère spécial"
-
-        errorList.appendChild(err)
-    } else {
-        password.classList.add("success")
-
-        if (password.value == password2.value) {
-            passwordRepeat.classList.add("success");
-        } else {
-            errorContainer.classList.add("visible");
-            passwordRepeat.classList.remove("success");
-            let err = document.createElement("li");
-            err.innerText = "Les mots de passe sont différents"
-
-            errorList.appendChild(err)
-        }
-    }
-
-    if (newsletterNon.checked || newsletterOui.checked) {
-        newsletterOui.classList.add("success");
-        newsletterNon.classList.add("success");
-    } else {
-        errorContainer.classList.add("visible")
-        let err = document.createElement("li");
-            err.innerText = "Veuillez choisir si vous souhaitez recevoir la newsletter"
-
-            errorList.appendChild(err)
-    }
-
-    successContainer.classList.remove("visible")
-    
-    if (pseudo.classList.contains("success") &&
-        email.classList.contains("success") &&
-        password.classList.contains("success") &&
-        passwordRepeat.classList.contains("success") &&
-        newsletterOui.classList.contains("success") &&
-        newsletterNon.classList.contains("success"))
-        {
+        errorList.replaceChildren()
         errorContainer.classList.remove("visible")
-        successContainer.classList.add("visible")
-    }
+        successContainer.classList.remove("visible")
+        loader.classList.remove("visible")
+
+        if (email.value == "") {
+            console.log("invalide")
+        } else {
+            email.classList.add("success")
+        }
+
+        if (pseudo.value.length < 6) {
+            errorContainer.classList.add("visible");
+            pseudo.classList.remove("success");
+
+            let err = document.createElement("li");
+            err.innerText = "Le champ pseudo doit contenir au moins 6 caractères";
+
+            errorList.appendChild(err);
+        } else {
+            pseudo.classList.add("success")
+        }
+
+        let passCheck = new RegExp(
+        "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$");
+
+        // Si le mdp n"a pas 10 caractère ou ne respecte pas la regex
+        if (password.value.length < 10 || passCheck.test(password.value) == false) {
+            errorContainer.classList.add("visible");
+            password.classList.remove("success");
+
+            let err = document.createElement("li");
+            err.innerText = "Le mot de passe doit faire 10 caractères minimum, contenir minuscule, majuscule, chiffre, caractère spécial"
+
+            errorList.appendChild(err)
+        } else {
+            password.classList.add("success")
+
+            if (password.value == password2.value) {
+                passwordRepeat.classList.add("success");
+            } else {
+                errorContainer.classList.add("visible");
+                passwordRepeat.classList.remove("success");
+                let err = document.createElement("li");
+                err.innerText = "Les mots de passe sont différents"
+
+                errorList.appendChild(err)
+            }
+        }
+
+        if (newsletterNon.checked || newsletterOui.checked) {
+            newsletterOui.classList.add("success");
+            newsletterNon.classList.add("success");
+        } else {
+            errorContainer.classList.add("visible")
+            let err = document.createElement("li");
+                err.innerText = "Veuillez choisir si vous souhaitez recevoir la newsletter"
+
+                errorList.appendChild(err)
+        }
+
+        successContainer.classList.remove("visible")
+        
+        if (pseudo.classList.contains("success") &&
+            email.classList.contains("success") &&
+            password.classList.contains("success") &&
+            passwordRepeat.classList.contains("success") &&
+            newsletterOui.classList.contains("success") &&
+            newsletterNon.classList.contains("success"))
+            {
+
+            errorContainer.classList.remove("visible")
+            successContainer.classList.add("visible")
+        }
+        }, 2000);
 })
+
 
 // DARK MODE
 function toggleDarkMode() {
